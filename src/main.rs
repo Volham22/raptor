5fb980d3e212d00a78b7f74ce52672c08c4bbb9b
Volham <corentin.machu@protonmail.com>
2022-08-17 23:06:10 +0200

    cli: Added config file as arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process;
 
+use clap::Parser;
 use vhosts::ListenerError;
 
 use crate::{
@@ -13,15 +14,25 @@ mod handlers;
 mod stream_utils;
 mod vhosts;
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+/// Raptor web server. Fast and light.
+struct Cli {
+    /// Config file for the server in json
+    config_file: String,
+}
+
 #[tokio::main]
 pub async fn main() -> ListenerError {
-    let config: Config = match Config::from_file::<ConfigError>("config.json").await {
+    let cli = Cli::parse();
+    let config: Config = match Config::from_file::<ConfigError>(&cli.config_file).await {
         Ok(c) => c,
         Err(msg) => {
             eprintln!("{}", msg);
             process::exit(1);
         }
     };
+
     println!("{:?}", config);
     let manager = Box::new(VhostManager::from_config(config));
 
