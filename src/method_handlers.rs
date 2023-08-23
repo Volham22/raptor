fd249577ec174e71a106c42057ceb2199f547ffa
Corentin Machu <corentin.machu@protonmail.com>
2023-08-23 15:05:05 +0200

    fix(http): Handle path traversal attempts

--- src/method_handlers.rs
+++ src/method_handlers.rs
@@ -32,11 +32,33 @@ fn resolve_path(conf: &Arc<Config>, uri: &[u8]) -> PathBuf {
     path
 }
 
+/// Check if the resolved path is `root_dir` or a child. If not we're outside
+/// the server root_dir. It's propably a path traversal attack attempt.
+async fn check_for_path_traversal(conf: &Arc<Config>, resolved_path: &Path) -> io::Result<bool> {
+    let absolute_path = fs::canonicalize(resolved_path).await?;
+    debug!("Absolute path: {:?}", absolute_path);
+    Ok(!absolute_path.starts_with(&conf.root_dir))
+}
+
 pub async fn handle_request<T: HttpRequest>(req: &T, conf: &Arc<Config>) -> Response {
     let resolved_path = resolve_path(conf, req.get_uri().as_ref().unwrap());
+
+    match check_for_path_traversal(conf, &resolved_path).await {
+        Ok(true) => {
+            return Response::from_io_result(
+                Err(io::ErrorKind::PermissionDenied.into()),
+                resolved_path.to_str().unwrap(),
+            );
+        }
+        Ok(false) => (),
+        Err(err) => {
+            return Response::from_io_result(Err(err), resolved_path.to_str().unwrap());
+        }
+    };
+
     match req.get_type().expect("Tried to handle invalid request") {
         RequestType::Get => Response::from_io_result(
-            handle_get(&resolved_path).await,
+            handle_get(&resolved_path).await.map(Some),
             resolved_path
                 .extension()
                 .map(|s| s.to_str().expect("Failed to convert from OsStr"))
