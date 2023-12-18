a96928c56b51ba921c57a0a65c4ab6ae7ca90631
Corentin Machu <corentin.machu@protonmail.com>
2023-12-18 21:00:33 +0100

    fix(connection): Replace faulty protocol log message
    * This message caused to unwrap an `Option` that can be `None`

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -18,14 +18,16 @@ pub async fn do_connection(
 ) -> io::Result<()> {
     let stream = ssl_socket.accept(client_socket).await?;
     let (_, conn) = stream.get_ref();
-    debug!(
-        "ALPN: {:?}",
-        String::from_utf8_lossy(conn.alpn_protocol().unwrap())
-    );
 
     match conn.alpn_protocol() {
-        Some(b"h2") => http2::do_http2(stream, config).await,
-        Some(b"http/1.1") => http11::do_http11(stream, config).await,
+        Some(b"h2") => {
+            debug!("Using 'h2' protocol");
+            http2::do_http2(stream, config).await
+        }
+        Some(b"http/1.1") => {
+            debug!("Using 'http/1.1' protocol");
+            http11::do_http11(stream, config).await
+        }
         Some(protocol) => {
             error!("Bad protocol received: '{:?}' closing connection", protocol);
             Ok(())
