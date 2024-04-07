66a2d29b31f9858147c41b8898f9f038d4c4b73e
Corentin Machu <corentin.machu@protonmail.com>
2024-04-07 17:13:02 +0200

    feat(http2): Add priority frame tests

--- http2/src/frames/priority.rs
+++ http2/src/frames/priority.rs
@@ -7,12 +7,7 @@ use super::{
 
 const PRIORITY_FRAME_SIZE: usize = 5;
 
-/// Server does not support priority frame yet.
-/// This function is only here to throw an error if an invalid frame has been received.
-pub(crate) async fn receive_priority_frame(
-    stream: &mut ConnectionStream,
-    frame: &Frame,
-) -> FrameResult<()> {
+pub(self) fn check_check_priority_frame(frame: &Frame) -> FrameResult<()> {
     if frame.length != PRIORITY_FRAME_SIZE as u32 {
         return Err(FrameError::BadPriorityFrameSize(frame.length));
     }
@@ -21,9 +16,74 @@ pub(crate) async fn receive_priority_frame(
         return Err(FrameError::PriorityFrameStreamZero);
     }
 
+    Ok(())
+}
+
+/// Server does not support priority frame yet.
+/// This function is only here to throw an error if an invalid frame has been received.
+pub(crate) async fn receive_priority_frame(
+    stream: &mut ConnectionStream,
+    frame: &Frame,
+) -> FrameResult<()> {
+    check_check_priority_frame(frame)?;
+
     let _ = utils::receive_n_bytes(stream, PRIORITY_FRAME_SIZE)
         .await
         .map_err(FrameError::IOError)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::frames::{errors::FrameError, Frame, FrameType};
+
+    use super::{check_check_priority_frame, PRIORITY_FRAME_SIZE};
+
+    #[test]
+    fn priority_non_zero_stream() {
+        let frame = Frame {
+            length: PRIORITY_FRAME_SIZE as u32,
+            flags: 0,
+            stream_id: 0,
+            frame_type: FrameType::Priority,
+        };
+
+        let result = check_check_priority_frame(&frame);
+        assert!(result.is_err());
+        match result.unwrap_err() {
+            FrameError::PriorityFrameStreamZero => (),
+            _ => panic!("Error is not correct"),
+        }
+    }
+
+    #[test]
+    fn priority_bad_length() {
+        let frame = Frame {
+            length: 51,
+            flags: 0,
+            stream_id: 0,
+            frame_type: FrameType::Priority,
+        };
+
+        let result = check_check_priority_frame(&frame);
+        assert!(result.is_err());
+        match result.unwrap_err() {
+            FrameError::BadPriorityFrameSize(51) => (),
+            _ => panic!("Error is not correct"),
+        }
+    }
+
+    #[test]
+    fn priority_correct_frame() {
+        let frame = Frame {
+            length: PRIORITY_FRAME_SIZE as u32,
+            flags: 0,
+            stream_id: 2,
+            frame_type: FrameType::Priority,
+        };
+
+        let result = check_check_priority_frame(&frame);
+        assert!(result.is_ok());
+    }
 }
